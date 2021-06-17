e4d5b614cea13966ac798c4e93e95c38596ff77c
Azeross <aziz.0101@mail.ru>
2021-06-17 17:05:40 +0600

    las update

--- TestProject/main.py
+++ TestProject/main.py
@@ -5,10 +5,4 @@ kfc = ['abaya2', 'baitur2','temer2','moskva2']
 list = [kfc, bk, mak]
 cafe = random.choice(list)
 
-if cafe == bk:
-    print('bk', random.choice(bk))
-elif cafe == mak:
-    print('mak', random.choice(mak))
-elif cafe == kfc:
-    print('kfc', random.choice(kfc))
-    else
+print(cafe, random.choice(bk))
