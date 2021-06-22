d8f29e26a2aabea9003ff9364c44d6117e7f9044
Azeross <aziz.0101@mail.ru>
2021-06-22 13:27:57 +0600

    Project GUI

--- Foodselecter/main.py
+++ Foodselecter/main.py
@@ -1,18 +1,47 @@
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import  QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
+import random
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+        self.setGeometry(500, 200, 500, 500)
+        self.setWindowTitle("Выбор еды")
+        self.main_text2 = QtWidgets.QLabel(self)
+        self.main_text1 = QtWidgets.QLabel(self)
+        self.main_text1.move(200, 150)
+        self.main_text2.move(200, 200)
+        self.btn = QtWidgets.QToolButton(self)
+        self.btn.move(200, 400)
+        self.btn.setText("Сгенерировать")
+        self.btn.adjustSize()
+        self.btn.clicked.connect(self.add_label)
+
+    def add_label(self):
+        bk = ['abaya', 'baitur', 'temer', 'moskva']
+        mak = ['abaya1', 'baitur1', 'temer1', 'moskva1']
+        kfc = ['abaya2', 'baitur2', 'temer2', 'moskva2']
+        lists = [kfc, bk, mak]
+        cafe = random.choice(lists)
+        for name, value in list(locals().items()):
+            if cafe is value:
+                values = random.choice(cafe)
+                self.main_text1.setText("Заведение: " + name)
+                self.main_text2.setText("Адресс: " + values)
+                self.main_text1.adjustSize()
+                self.main_text2.adjustSize()
+                #print(name, random.choice(cafe))
+                break
+
 
 def application():
     app = QApplication(sys.argv)
-    window = QMainWindow()
-    window.setGeometry(500,200, 500,500)
-    window.setWindowTitle("Выбор еды")
-    main_text = QtWidgets.QLabel(window)
-    main_text.setText("Хватит жрать123456789")
-    main_text.move(200,250)
-    main_text.adjustSize()
+    window = Window()
     window.show()
     sys.exit(app.exec_())
+
 
 if __name__ == "__main__":
     application()
